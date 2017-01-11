27a9d1649abbc876e2970cd1f4dd39fff8673637
Roman Boboshko <romanboboshko89@gmail.com>
2017-01-11 18:40:11 +0200

    Add auth controller and start user logic

---
+++ server/controllers/authentification.js
@@ -0,0 +1,17 @@
+const User = require('../models/user');
+
+exports.signup = function(req, res, next) {
+    const email = req.body.email;
+    const password = req.body.password;
+
+    // See if a user with the given email exists
+    User.findOne({ email: email }, function(err, existingUser) {
+        
+    })
+
+    // If a user with email does exist, return an error
+
+    // If a user with email does not exist, create and save user record
+
+    // Respond to request indicating the user was created
+}
