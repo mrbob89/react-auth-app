387142019c3de42017463638e4d145772098e123
Roman Boboshko <romanboboshko89@gmail.com>
2017-01-11 19:14:08 +0200

    Add more logic to signup

--- server/controllers/authentification.js
+++ server/controllers/authentification.js
@@ -4,14 +4,30 @@ exports.signup = function(req, res, next) {
     const email = req.body.email;
     const password = req.body.password;
 
+    if (!email || !password) {
+        return res.status(422).send({ error: 'You must provide email and password' });
+    }
+
     // See if a user with the given email exists
     User.findOne({ email: email }, function(err, existingUser) {
-        
+        if (err) { return next(err); }
+
+        // If a user with email does exist, return an error
+        if (existingUser) {
+            return res.status(422).send({ error: 'Email is in use' });
+        }
+
+        // If a user with email does not exist, create and save user record
+        const user = new User({
+            email: email,
+            password: password
+        });
+
+        user.save(function(err) {
+            if (err) { return next(err); }
+
+            // Respond to request indicating the user was created
+            res.json({ success: true });
+        });
     })
-
-    // If a user with email does exist, return an error
-
-    // If a user with email does not exist, create and save user record
-
-    // Respond to request indicating the user was created
 }
