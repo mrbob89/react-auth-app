5d90ab88bcc27186538920bc2036c3c4dd8df900
Roman Boboshko <romanboboshko89@gmail.com>
2017-01-11 19:47:48 +0200

    Create JWT token

--- server/models/user.js
+++ server/models/user.js
@@ -15,10 +15,12 @@ userSchema.pre('save', function(next) {
 
     bcrypt.genSalt(10, function (err, salt) {
         if (err) { return next(err) }
-
+        
+        // Hash (encrypt) our password using the salt
         bcrypt.hash(user.password, salt, null, function(err, hash) {
             if (err) { return next(err) }
-
+            
+            // overwrite plain text password with encrypted password
             user.password = hash;
             next();
         });
