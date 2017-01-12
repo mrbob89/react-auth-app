26d60e0e84a415fe466ce0b11d337f385fa1d200
Roman Boboshko <romanboboshko89@gmail.com>
2017-01-12 11:54:19 +0200

    Add example of check auth in root route

--- server/router.js
+++ server/router.js
@@ -1,5 +1,12 @@
 const Authentification = require('./controllers/authentification');
+const passportService = require('./services/passport');
+const passport = require('passport');
+
+const requireAuth = passport.authenticate('jwt', { session: false });
 
 module.exports = function(app) {
+    app.get('/', requireAuth, function(req, res) {
+        res.send({ hi: 'there' });
+    });
     app.post('/signup', Authentification.signup);
 }
