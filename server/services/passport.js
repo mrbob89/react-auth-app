18d5e31a090d5a9addb76721321cbf9fa9547309
Roman Boboshko <romanboboshko89@gmail.com>
2017-01-12 12:19:55 +0200

    Add signin functionality to server

--- server/services/passport.js
+++ server/services/passport.js
@@ -14,6 +14,12 @@ const localLogin = new LocalStrategy(localOptions, function (email, password, do
         if (!user) { return done(null, false); }
 
         // compare passwords - is 'password' equal to user.password?
+        user.comparePassword(password, function(err, isMatch) {
+            if (err) { return done(err); }
+            if (!isMatch) { return done(null, false); }
+
+            return done(null, user);
+        });
     });
 });
 
@@ -39,3 +45,4 @@ const jwtLogin = new JwtStrategy(jwtOptions, function(payload, done) {
 
 // Tell passport to use this strategy
 passport.use(jwtLogin);
+passport.use(localLogin);
