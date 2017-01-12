a7a623e620803a35fdb935fa03a465afa626248c
Roman Boboshko <romanboboshko89@gmail.com>
2017-01-12 16:59:27 +0200

    Save token to local storage + add auth reducer

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import { browserHistory } from 'react-router'; 
+import { AUTH_USER } from './types';
 
 const ROOT_URL = 'http://localhost:3090';
 
@@ -8,6 +9,9 @@ export function signinUser({ email, password }) {
         // Submit email/password to the server
         axios.post(`${ROOT_URL}/signin`, { email, password })
             .then(response => {
+                dispatch({ type: AUTH_USER });
+                // Save the JWT token
+                localStorage.setItem('token', response.data.token);
                 // Redirect to /feature page
                 browserHistory.push('/feature');
             })
